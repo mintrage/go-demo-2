2e4ae3e8b192e206e3553daff50c7dd8279516da
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-07-02 23:38:29 +0300

    6.4. Cap и len Array и Slice

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	transactions := [5]int{1, 2, 3, 4, 5}
-	banks := [2]string{}
+	transactions := [6]int{1, 2, 3, 4, 5, 6}
+	transactionsPartial := transactions[1:5]
+	transactionsNewPartial := transactionsPartial[:1]
+	transactionsNewPartial[0] = 30
+	transactionsNewPartial = transactionsNewPartial[0:4]
+	// banks := [2]string{}
 
-	fmt.Println(transactions[1])
-	banks[0] = "Тинькофф"
-	fmt.Println(banks)
-	partial := transactions[1:]
-	fmt.Println(partial)
+	fmt.Println(transactions)
+	// banks[0] = "Тинькофф"
+	// fmt.Println(banks)
+	// partial := transactions[1:]
+	// fmt.Println(partial)
+	fmt.Println(transactionsNewPartial)
+	fmt.Println(len(transactionsPartial), cap(transactionsPartial))
+	fmt.Println(len(transactionsNewPartial), cap(transactionsNewPartial))
 }
