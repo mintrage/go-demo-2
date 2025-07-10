aee4281f31557bc9bf8a2897b20669d402112c37
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-07-10 19:00:34 +0300

    6.5. Динамические массивы Array и Slice

--- main.go
+++ main.go
@@ -3,19 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	transactions := [6]int{1, 2, 3, 4, 5, 6}
-	transactionsPartial := transactions[1:5]
-	transactionsNewPartial := transactionsPartial[:1]
-	transactionsNewPartial[0] = 30
-	transactionsNewPartial = transactionsNewPartial[0:4]
-	// banks := [2]string{}
-
+	transactions := []int{0, 20, 35}
+	temp := transactions
+	transactions = append(transactions, 100)
+	fmt.Println(temp)
 	fmt.Println(transactions)
-	// banks[0] = "Тинькофф"
-	// fmt.Println(banks)
-	// partial := transactions[1:]
-	// fmt.Println(partial)
-	fmt.Println(transactionsNewPartial)
-	fmt.Println(len(transactionsPartial), cap(transactionsPartial))
-	fmt.Println(len(transactionsNewPartial), cap(transactionsNewPartial))
 }
